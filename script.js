ac6618632aa61ff81567399f1debdad8daf546ec
PawelK <pawo161@gmail.com>
2019-07-12 11:06:26 +0000

    files

---
+++ script.js
@@ -0,0 +1,68 @@
+const video = document.getElementById("myvideo");
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+const audio = document.getElementById("audio")
+let trackButton = document.getElementById("trackbutton");
+let updateNote = document.getElementById("updatenote");
+
+let isVideo = false;
+let model = null;
+
+const modelParams = {
+    flipHorizontal: true,   // flip e.g for video  
+    maxNumBoxes: 4,        // maximum number of boxes to detect
+    iouThreshold: 0.5,      // ioU threshold for non-max suppression
+    scoreThreshold: 0.7,    // confidence threshold for predictions.
+}
+
+function startVideo() {
+    handTrack.startVideo(video).then(function (status) {
+        console.log("video started", status);
+        if (status) {
+            updateNote.innerText = "Wideo ruszyło! Poszukuję dłoni."
+            isVideo = true
+            setInterval(runDetection(), 800);
+        } else {
+            updateNote.innerText = "Proszę włącz obraz..."
+        }
+    });
+}
+
+function toggleVideo() {
+    if (!isVideo) {
+        updateNote.innerText = "Startuję wideo..."
+        startVideo();
+    } else {
+        updateNote.innerText = "Stopuję..."
+        handTrack.stopVideo(video)
+        isVideo = false;
+        updateNote.innerText = "Zastopowane..."
+    }
+}
+
+
+
+function runDetection() {
+    model.detect(video).then(predictions => {
+        console.log("Predictions: ", predictions);
+        model.renderPredictions(predictions, canvas, context, video);
+        if (isVideo) {
+            requestAnimationFrame(runDetection);
+        }
+        if(predictions.length > 0){
+                audio.play();
+                
+            }
+            else{
+                audio.pause();
+            }
+    });
+}
+
+// Load the model.
+handTrack.load(modelParams).then(lmodel => {
+    // detect objects in the image.
+    model = lmodel
+    updateNote.innerText = "Model załadowany (ML)"
+    trackButton.disabled = false
+});
