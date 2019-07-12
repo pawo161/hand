c30b5b9030a176e7ad5bece0859b834a6cd71946
PawelK <pawo161@gmail.com>
2019-07-12 22:36:23 +0000

    new

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ function toggleVideo() {
 
 function runDetection() {
     model.detect(video).then(predictions => {
-        console.log("Predictions: ", predictions);
+        //console.log("Predictions: ", predictions);
         model.renderPredictions(predictions, canvas, context, video);
         if (isVideo) {
             requestAnimationFrame(runDetection);
