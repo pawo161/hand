4892257ca09a70a2b7cfc8839f8146a94d36796e
pawel <48882583+pawo161@users.noreply.github.com>
2019-07-12 11:21:52 +0000

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const modelParams = {
     flipHorizontal: true,   // flip e.g for video  
     maxNumBoxes: 4,        // maximum number of boxes to detect
     iouThreshold: 0.5,      // ioU threshold for non-max suppression
-    scoreThreshold: 0.55,    // confidence threshold for predictions.
+    scoreThreshold: 0.62,    // confidence threshold for predictions.
 }
 
 function startVideo() {
