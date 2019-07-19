631e1a958372ab8afcd9a6df2cf8ec118eba95ae
PawelK <pawo161@gmail.com>
2019-07-19 00:49:52 +0000

    teytwewe

--- script.js
+++ script.js
@@ -287,23 +287,3 @@ function download() {
   }, 100);
 }
 
-$.fn.animateRotate = function(angle, duration, easing, complete) {
-  var args = $.speed(duration, easing, complete);
-  var step = args.step;
-  return this.each(function(i, e) {
-    args.complete = $.proxy(args.complete, e);
-    args.step = function(now) {
-      $.style(e, 'transform', 'rotate(' + now + 'deg)');
-      if (step) return step.apply(e, arguments);
-    };
-
-    $({deg: 0}).animate({deg: angle}, args);
-  });
-};
-
-$("#tx").animateRotate(360, {
-  duration: 99999,
-  easing: 'linear',
-  complete: function () {},
-  step: function () {}
-});
